57876c36c4902c00df213c94d52f20fcf0ac0b98
Fisher <fishermedders@gmail.com>
2026-02-10 22:37:35 -0500

    try for ios fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,26 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="hero-sky"></div>
-      <div className="hero-background"></div>
-      <div className="hero-clouds">
-        <img src="/assets/hero-clouds.png"></img>
+      <div className="stage">
+        <div className="hero-sky"></div>
+        <div className="hero-background"></div>
+        <div className="hero-clouds">
+          <img src="/assets/hero-clouds.png"></img>
+        </div>
+        <div className="hero-logo">
+          <img src="/assets/tour-logo.png" />
+          <img src="/assets/tour-plane.png" />
+        </div>
+        <nav className="navbar">
+          <ul>
+            <li>Home</li>
+            <li>Tour/Tix</li>
+            <li>Merch</li>
+            <li>Booking</li>
+            <li>Gallery</li>
+          </ul>
+        </nav>
       </div>
-      <div className="hero-logo">
-        <img src="/assets/tour-logo.png" />
-        <img src="/assets/tour-plane.png" />
-      </div>
-      <nav className="navbar">
-        <ul>
-          <li>Home</li>
-          <li>Tour/Tix</li>
-          <li>Merch</li>
-          <li>Booking</li>
-          <li>Gallery</li>
-        </ul>
-      </nav>
     </>
   );
 }
